ee119fab10ff240a7451ee45351b31fe23100942
Janit Yadav <janityadav16@gmail.com>
2024-11-05 21:41:47 +0530

    new project with tailwind

--- App.js
+++ App.js
@@ -1,71 +1,13 @@
-import React, { useState } from 'react';
+import React from 'react';
+import FormComponent from './FormComponent';
 
-function form() {
-  const [formData, setformData] = useState({
-    firstname: '',
-    lastname: '',
-    contactNumber: '',
-    email: ''
-  });
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setformData({
-      ...formData,
-      [name]: value
-    });
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log('Form submitted:', formData);
-  };
-
+function App() {
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label>First Name:</label>
-        <input
-          type="text"
-          name="firstname"
-          value={formData.firstname}
-          onChange={handleChange}
-          required
-        />
-      </div>
-      <div>
-        <label>Last Name:</label>
-        <input
-          type="text"
-          name="lastname"
-          value={formData.lastname}
-          onChange={handleChange}
-          required
-        />
-      </div>
-      <div>
-        <label>Contact Number:</label>
-        <input
-          type="tel"
-          name="contactNumber"
-          value={formData.contactNumber}
-          onChange={handleChange}
-          required
-        />
-      </div>
-      <div>
-        <label>Email:</label>
-        <input
-          type="email"
-          name="email"
-          value={formData.email}
-          onChange={handleChange}
-          required
-        />
-      </div>
-      <button type="submit">Submit</button>
-    </form>
+    <div>
+      <h1>React Form</h1>
+      <FormComponent />
+    </div>
   );
 }
 
-export default form;
+export default App;
